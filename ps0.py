89f52eef7245e192693b9460403195fb27b8aabb
madhavid1967 <madhavid1967@yahoo.com>
2015-04-01 21:50:07 -0700

    first homework assignment completed

---
+++ ps0.py
@@ -0,0 +1,8 @@
+#Problems set 0
+#Name Madhavi Desai
+#Time 10:17AM
+
+lName = raw_input('Enter your last name.\n')
+fName = raw_input('Enter your first name.\n')
+print(fName)
+print(lName)
---
+++ ps1a.py
@@ -0,0 +1,52 @@
+#Problem set 1a
+#Name Madhavi Desai
+#Time 12:20PM
+
+nextOdd = 3
+numList = []
+primeList = []
+divisior = 1
+countDivisior = 0
+primeCount = 0
+column_pad = 6
+max_column = 6
+column = 1
+
+#Create an array of odd numbers from 1 to 10000
+while(nextOdd < 10000):
+	if(nextOdd%2 != 0):
+		numList.append(nextOdd)
+	nextOdd = nextOdd + 1
+
+#Initialize n with first number from odd numbers list.	
+n = numList[0]
+
+#Since we know that 2 is prime number, add it to primeList array.
+primeList.append(2)
+print str(2).center(column_pad),
+
+for n in numList:
+	#Find number of divisiors for number n.
+	while(divisior <= n):
+		if(n%divisior == 0):
+			countDivisior = countDivisior + 1
+		divisior = divisior + 1
+	
+	#If there are only 2 divisiors for number n then it is 
+	#prime number and add it to the primeList array.	
+	if(countDivisior == 2):
+		primeList.append(n)
+		print str(n).center(column_pad),
+		column += 1
+		if column == max_column:
+			print
+			column = 1
+		primeCount = primeCount + 1
+		lastPrime = n
+	divisior = 1
+	countDivisior = 0
+	if (primeCount == 999):
+		break
+print
+print
+print '1000th prime number is',lastPrime 
